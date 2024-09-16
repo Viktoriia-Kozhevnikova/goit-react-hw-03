cb69439dab2eb14f9b488a0a5320af09b62f82f1
Viktoriia-Kozhevnikova <161330605+Viktoriia-Kozhevnikova@users.noreply.github.com>
2024-09-16 13:17:12 +0300

    add index file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,20 +1,22 @@
-// import React from 'react'
-import s from '../Contact/Contact.module.css'
+import s from '../Contact/Contact.module.css';
 import { HiUser, HiPhone } from "react-icons/hi";
 
-const Contact = ({name, number}) => {
+const Contact = ({ name, number, onDelete }) => {
   return (
     <div className={s.container}>
-      <p className={s.text}>
-        <HiUser className={s.icon} />
-        {name}
-      </p>
-      <p className={s.text}>
-        <HiPhone className={s.icon} />
-        {number}
-      </p>
+      <div className={s.contact}>
+        <p className={s.text}> 
+          <HiUser className={s.icon} />
+          {name}
+        </p>
+        <p className={s.text}>
+          <HiPhone className={s.icon} />
+          {number}
+        </p>
       </div>
-  )
-}
+      <button onClick={onDelete} className={s.btn}>Delete</button>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,7 @@
-// import React from 'react'
-import s from '../ContactList/ContactList.module.css'
-import Contact from '/src/components/Contact/Contact.jsx'
+import s from '../ContactList/ContactList.module.css';
+import Contact from '/src/components/Contact/Contact.jsx';
 
-const ContactList = ({contacts, handleDeleteContact}) => {
+const ContactList = ({ contacts, handleDeleteContact }) => {
   return (
     <ul className={s.list}>
       {contacts.map(contact => (
@@ -10,12 +9,12 @@ const ContactList = ({contacts, handleDeleteContact}) => {
           <Contact
             name={contact.name}
             number={contact.number}
+            onDelete={() => handleDeleteContact(contact.id)}
           />
-          <button onClick={() => handleDeleteContact(contact.id)} className={s.btn}>Delete</button>
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
 export default ContactList;
